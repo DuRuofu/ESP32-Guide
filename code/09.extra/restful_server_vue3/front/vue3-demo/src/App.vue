30a8e6f62e581aedae36b7c57058a8c106dfbc2e
DuRuofu <duruofu@qq.com>
2025-02-17 17:02:38 +0800

    update: 添加Web服务Vue3示例

---
+++ code/09.extra/restful_server_vue3/front/vue3-demo/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="page">
+    <h1>version</h1>
+    <p>{{ version }}</p>
+    <el-button type="primary" @click="getVersionButton">Primary</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue'
+import axios from 'axios';
+const version = ref('')
+
+const getVersionButton = () => {
+
+  axios.get('/api/v1/system/info').then((res) => {
+    version.value = res.data.version
+    console.log("获取成功",res)
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+</script>
+
+<style scoped lang="scss">
+.page {
+  text-align: center;
+  margin: 20px;
+}
+
+.page h1 {
+  font-size: 2em;
+  margin-bottom: 10px;
+}
+
+.page p {
+  font-size: 1.2em;
+}
+</style>
